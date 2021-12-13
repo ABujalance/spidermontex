55dd478b13402f546c644492bc2eb46c0b107b19
Alberto Bujalance <abuj@ciklum.com>
2021-12-13 18:01:56 +0100

    la broma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+const calculateTimeLeft = () => {
+  const difference = +new Date(`12/16/2021 21:45`) - +new Date();
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60)
+    };
+  }
+  return timeLeft;
+};
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span className='time'>
+        {timeLeft[interval]} {interval}{" "}
+        <br></br>
+      </span>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='content'>
+        <h1 className='title'>SPIDERMONTEX</h1>
+        {timerComponents.length ? timerComponents : <span className='end'>MI SENTIDO ARÁCNIDO SE HA ACTIVADO</span>}
+      </div>
     </div>
   );
 }
