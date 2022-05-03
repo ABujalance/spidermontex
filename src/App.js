3f6d72e8dffd859de17edcbb5180207de71d453f
Alberto Bujalance <abuj@ciklum.com>
2022-05-03 11:29:37 +0200

    fafaverso update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import "./App.css";
 import React, { useEffect, useState } from "react";
 
 const calculateTimeLeft = () => {
-  const difference = +new Date(`12/16/2021 21:45`) - +new Date();
+  const difference = +new Date(`05/06/2022 21:15`) - +new Date();
   let timeLeft = {};
 
   if (difference > 0) {
@@ -10,7 +10,7 @@ const calculateTimeLeft = () => {
       days: Math.floor(difference / (1000 * 60 * 60 * 24)),
       hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
       minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60)
+      seconds: Math.floor((difference / 1000) % 60),
     };
   }
   return timeLeft;
@@ -34,18 +34,17 @@ function App() {
     }
 
     timerComponents.push(
-      <span className='time'>
-        {timeLeft[interval]} {interval}{" "}
-        <br></br>
+      <span className="time">
+        {timeLeft[interval]} {interval} <br></br>
       </span>
     );
   });
 
   return (
-    <div className='container'>
-      <div className='content'>
-        <h1 className='title'>SPIDERMONTEX</h1>
-        {timerComponents.length ? timerComponents : <span className='end'>MI SENTIDO ARÁCNIDO SE HA ACTIVADO</span>}
+    <div className="container">
+      <div className="content">
+        <h1 className="title">Fafaverso of Madness</h1>
+        {timerComponents.length && timerComponents}
       </div>
     </div>
   );
